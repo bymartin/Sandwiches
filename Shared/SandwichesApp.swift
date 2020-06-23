53e175075640956ab63c54cc2fdd88af11b303df
Barry Martin <bymartin@users.noreply.github.com>
2020-06-23 12:40:13 -0500

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Barry Martin on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
